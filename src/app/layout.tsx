0bb0d866f74fbf62e2a1dee00de5e483eee4ec77
Remona-B <itsremona1101@gmail.com>
2025-10-20 05:17:09 +0530

    Initial commit - job portal app with db connection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
+
 import '@mantine/core/styles.css';
 
 import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
